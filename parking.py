b003e305547d7d0e730ddf16333faa9edabd1a00
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-06-21 12:36:41 +0100

    makes allowance for sidecase of no cars entering carpark

--- parking.py
+++ parking.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 from math import ceil
 
-file_name = 'parking.log'
+file_name = 'parking2.log'
 file = open(file_name, 'r') 
 parking_log = {}
 data_order = ['date', 'type', 'direction']
 
+# Look through each line of the log and extract the data 
 
 for line in file.readlines():
 
     details = line.split(',')
     details = [x.strip() for x in details]
+    # Remove the unique ref
     vehicle_ref = details.pop(1)
 
+    # Retrieve data from log if present else crate map for data 
     vehicle_data = parking_log[vehicle_ref] if vehicle_ref in parking_log else {'IN/OUT': [], 'TOTAL' : []}
-    
+
+    # Build entry/exit data, append to the correct key and update log
     direction_data = {key:value for key, value in zip(data_order, details)}
 
     if direction_data['direction'] == 'Entry':
@@ -36,6 +40,8 @@ file.close()
 
 def average_time ():
     total_car_time_list = []
+
+    # Iterate through list to remove only car data
     for x in parking_log:
         is_it_a_car = parking_log[x]['IN/OUT'][0]['IN']['type'] == 'Car'
 
@@ -44,6 +50,11 @@ def average_time ():
 
             total_car_time_list.append(y.seconds)
 
+    # If there are no cars return error
+    if len(total_car_time_list) == 0:
+        return print('No cars have entered the carpark')
+
+    # Otherwise return average stay
     average_time = ceil((sum(total_car_time_list) / len(total_car_time_list)) / 60)
     print('The average time a car used the carpark for was {} minutes'.format(average_time))   
 
